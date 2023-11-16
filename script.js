cd49c251066dd28f15f38b9ea896f7a0cc828c6e
gustavo <gusgurtavo@gmail.com>
2023-11-16 20:43:45 -0300

    add initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    new Sortable(document.getElementById('sortable-list1'), {
+        swap: true, // Enable swap plugin
+        animation: 150,
+        ghostClass: 'sortable-ghost'
+    });
+});
+
+$(document).ready(function() {
+    new Sortable(document.getElementById('sortable-list2'), {
+        // swap: true, // Enable swap plugin
+        animation: 150,
+        ghostClass: 'sortable-ghost'
+    });
+});
